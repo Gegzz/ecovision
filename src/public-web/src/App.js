a011166f09c483bb66d28ee68eeda42b2743ff60
Gegzz <ggegeshidze95@gmail.com>
2020-10-13 02:11:54 +0400

    added bulma

--- src/public-web/src/App.js
+++ src/public-web/src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Section, Container } from 'react-bulma-components'
+import Navbar from './components/Navbar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default () => (
+  <>
+    <Section>
 
-export default App;
+    </Section>
+    <Section>
+      <Container>
+        <Navbar />
+      </Container>
+    </Section>
+  </>
+)

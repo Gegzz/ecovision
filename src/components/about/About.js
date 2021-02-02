28ba17c3dcb4b59d290e13f9a43bbf50e6ebe089
Gegzz <ggegeshidze95@gmail.com>
2021-02-02 18:56:08 +0400

    added .prettierrc, corrected news page (About.js, needs rename)

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,87 +1,113 @@
-import React from "react";
-import { Section, Box, Image, Media } from "react-bulma-components";
-import { HeadingWithBackground, Space } from "..";
-import { ParallaxImage } from "../../assets";
-import Tile from "../Tile";
-import Tilt from "react-parallax-tilt";
+import React from 'react'
+import { Section, Box, Media, Heading } from 'react-bulma-components'
+import { HeadingWithBackground, Space } from '..'
+import { ParallaxImage } from '../../assets'
+import Tile from '../Tile'
+import { colors } from '../../consts'
 
-const max = 4;
-const tiltEnabled = true;
+const max = 4
+const tiltEnabled = true
 
 const aboutData = [
   {
-    title: "12 ways to travel sustainably in the new year",
+    title: '12 ways to travel sustainably in the new year',
     picture:
-      "https://www.nationalgeographic.com/content/dam/ngdotcom/rights-exempt/homepage/hero-crops/sustainable-travel-newyear-newcrop_14.adapt.1190.1.jpg",
+      'https://www.nationalgeographic.com/content/dam/ngdotcom/rights-exempt/homepage/hero-crops/sustainable-travel-newyear-newcrop_14.adapt.1190.1.jpg'
   },
   {
-    title: "Why whale watching is having a moment—in New York City",
+    title: 'Why whale watching is having a moment—in New York City',
     picture:
-      "https://www.nationalgeographic.com/content/dam/travel/2021-digital/whale-watching-nyc/celia-ackerman_for-nat-geo_9559.adapt.590.1.jpg",
+      'https://www.nationalgeographic.com/content/dam/travel/2021-digital/whale-watching-nyc/celia-ackerman_for-nat-geo_9559.adapt.590.1.jpg'
   },
   {
-    title: "What vaccines mean for the return of travel",
+    title: 'What vaccines mean for the return of travel',
     picture:
-      "https://www.nationalgeographic.com/content/dam/travel/2021-digital/travel-poll/gettyimages-1230244722.adapt.590.1.jpg",
+      'https://www.nationalgeographic.com/content/dam/travel/2021-digital/travel-poll/gettyimages-1230244722.adapt.590.1.jpg'
   },
   {
-    title: "How viruses shape our world",
+    title: 'How viruses shape our world',
     picture:
-      "https://www.nationalgeographic.com/content/dam/magazine/rights-exempt/2021/02/viruses/viruses-embryo-og.adapt.1190.1.jpg",
-  },
-];
+      'https://www.nationalgeographic.com/content/dam/magazine/rights-exempt/2021/02/viruses/viruses-embryo-og.adapt.1190.1.jpg'
+  }
+]
 
 const NewsItem = ({ title, description, picture }) => (
   <Box
-    style={{ backgroundColor: "rgba(255,255,255,0.6)" }}
-    paddingless
-    radiusless
-  >
-    <Media>
-      <Media.Item position="left">
-        <figure class="image" style={{ height: 256, width: 256, backgroundImage: `url(${picture})`, backgroundSize: 'cover' }} />
-      </Media.Item>
-      <Media.Content style={{padding: 24, paddingLeft: 8}}>
-        <p>
-          <strong>{title}</strong> <small>@johnsmith</small> <small>31m</small>
-          <br></br>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-          efficitur sit amet massa fringilla egestas. Nullam condimentum luctus
-          turpis.
-        </p>
-      </Media.Content>
-    </Media>
+    style={{
+      height: 512,
+      backgroundImage: `url(${picture})`,
+      backgroundSize: 'cover',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'flex-end',
+      width: '100%'
+    }}
+    radiusless>
+    <div style={{ width: '100%' }}>
+      <Heading style={{ color: colors.background, fontSize: 40 }}>
+        {title}
+      </Heading>
+      <p style={{ color: colors.background }}>{description}</p>
+    </div>
   </Box>
-);
+)
 
 const About = () => (
   <Section
     style={{
       backgroundImage: `url(${ParallaxImage})`,
-      backgroundAttachment: "fixed",
-      paddingVertical: 200,
-    }}
-  >
-    <Space />
+      backgroundAttachment: 'fixed',
+      paddingVertical: 200
+    }}>
+    <Space>
+      <Heading
+        style={{ textAlign: 'center', color: colors.background }}
+        size={2}
+        weight="bold">
+        Recent News
+      </Heading>
+    </Space>
     <Tile kind="ancestor">
-        <Tile kind="parent" size={2} />
-        <Tile kind="parent" size={8} style={{ backgroundOpacity: 50 }}>
-            {/* <Tilt transitionSpeed={2000} tiltMaxAngleX={max} tiltMaxAngleY={max} tiltEnable={tiltEnabled}> */}
-            <div>
-            {aboutData.map((data, index) => (
-                <NewsItem
-                key={index}
-                title={data.title}
-                description={data.description}
-                picture={data.picture}
-                />
-            ))}
-            </div>
-            {/* </Tilt> */}
-        </Tile>
+      <Tile kind="parent" size={2} />
+      <Tile
+        kind="parent"
+        size={8}
+        style={{
+          backgroundOpacity: 50,
+          display: 'flex',
+          flexDirection: 'column'
+        }}>
+        {/* <Tilt transitionSpeed={2000} tiltMaxAngleX={max} tiltMaxAngleY={max} tiltEnable={tiltEnabled}> */}
+        {[aboutData[0]].map((data, index) => (
+          <NewsItem
+            key={index}
+            title={data.title}
+            description={data.description}
+            picture={data.picture}
+          />
+        ))}
+        {/* </Tilt> */}
+      </Tile>
+    </Tile>
+    <Tile kind="ancestor" size={8}>
+      {aboutData
+        .reduce(function (result, value, index, array) {
+          if (index % 2 === 0) result.push(array.slice(index, index + 2))
+          return result
+        }, [])
+        .map((data, index) => (
+          <Tile kind="parent" size={6}>
+            <NewsItem
+              key={index}
+              title={data.title}
+              description={data.description}
+              picture={data.picture}
+            />
+          </Tile>
+        ))}
     </Tile>
     <Space />
   </Section>
-);
+)
 
-export default About;
+export default About
---
+++ src/consts.js
@@ -0,0 +1,5 @@
+const colors = {
+  background: "#EBEBE9",
+};
+
+export { colors };
